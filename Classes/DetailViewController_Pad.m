a17fd9a0b3dbbf3ac224bc40ed1880f86ab50ea2
Cory D. Wiles <kwylez@gmail.com>
2010-11-10 15:42:39 -0600

    implementation of new iad management methods

--- Classes/DetailViewController_Pad.m
+++ Classes/DetailViewController_Pad.m
@@ -58,6 +58,8 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
   
+  [tableView deselectRowAtIndexPath:indexPath animated:YES];
+  
   SubDetailViewController_iPad *subDetail = [[SubDetailViewController_iPad alloc] init];
   UINavigationController *nav             = [[UINavigationController alloc] initWithRootViewController:subDetail];
 
@@ -76,6 +78,10 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
   return YES;
+}
+
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+  [self fixupAdView:toInterfaceOrientation];
 }
 
 - (void)populateTable {
@@ -108,6 +114,91 @@
   [self.toolbar setItems:[NSArray array] animated:YES];
   
   self.popoverController = nil;
+}
+
+- (void)fixupAdView:(UIInterfaceOrientation)toInterfaceOrientation {
+  
+  if (adBannerView != nil) {
+    
+    [super configureIAdContentSizes];
+    
+    [UIView beginAnimations:@"fixupViews" context:nil];
+    
+    CGFloat toolbarHeight = self.toolbar.frame.size.height;
+    
+    if (adBannerViewIsVisible) {
+      
+      CGRect adBannerViewFrame = [adBannerView frame];
+      CGRect contentViewFrame  = self.tblView.frame;
+      
+      adBannerViewFrame.origin.x = 0;
+      adBannerViewFrame.origin.y = 0 + toolbarHeight;
+      
+      [adBannerView setFrame:adBannerViewFrame];
+      
+      contentViewFrame.origin.y    = adBannerViewFrame.origin.y + 5 + [self getBannerHeight];
+      contentViewFrame.size.height = self.tblView.frame.size.height - [self getBannerHeight];
+      
+      self.tblView.frame = contentViewFrame;
+      
+    } else {
+      
+      CGRect adBannerViewFrame = [adBannerView frame];
+      
+      adBannerViewFrame.origin.x = 0;
+      adBannerViewFrame.origin.y = -([self getBannerHeight:toInterfaceOrientation] + toolbarHeight);
+      
+      [adBannerView setFrame:adBannerViewFrame];
+      
+      CGRect contentViewFrame = self.tblView.frame;
+      
+      contentViewFrame.origin.y    = 0 + toolbarHeight;
+      contentViewFrame.size.height = self.tblView.frame.size.height;
+      
+      self.tblView.frame = contentViewFrame;            
+    }
+    
+    [UIView commitAnimations];
+  }   
+}
+
+- (int)getBannerHeight:(UIDeviceOrientation)orientation {
+
+  /**
+   * For some reason when the device rotates to landscape the landscape version 
+   * of the iAd isn't respected, probably due to the use of a splitViewController, 
+   * and the table view 'y' coordinates isn't consistant.
+   *
+   * So in order to 'fix' this issue I always return 32
+   */
+  return kPortraitIAdHeight;
+}
+
+- (void)configureIAdContentSizes {
+  
+  float version = [[[UIDevice currentDevice] systemVersion] floatValue];
+  
+  if ((version >= 4.000000) && (version <= 4.100000)) {
+    
+    [adBannerView setRequiredContentSizeIdentifiers:[NSSet setWithObjects:ADBannerContentSizeIdentifier320x50, ADBannerContentSizeIdentifier480x32, nil]];
+    
+    if (UIInterfaceOrientationIsLandscape([UIDevice currentDevice].orientation)) {
+      [adBannerView setCurrentContentSizeIdentifier:ADBannerContentSizeIdentifier480x32];
+    } else {
+      [adBannerView setCurrentContentSizeIdentifier:ADBannerContentSizeIdentifier320x50];            
+    }
+    
+  } else {
+    
+    /**
+     * Once again I am forcing the iAd portrait version because of the 
+     * splitviewcontroller and view sizes of the 'detail' controller.  I don't know
+     * yet if this is a developer problem or a bug with Apple
+     */
+    [adBannerView setRequiredContentSizeIdentifiers:[NSSet setWithObjects:ADBannerContentSizeIdentifierPortrait,nil]];
+    
+    [adBannerView setCurrentContentSizeIdentifier:ADBannerContentSizeIdentifierPortrait];
+  }
 }
 
 @end
