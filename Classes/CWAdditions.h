a38635b63c7f3c7aaac3d886c4716e90b7404541
Cory D. Wiles <kwylez@gmail.com>
2011-06-01 10:50:34 -0400

    initial import

---
+++ Classes/CWAdditions.h
@@ -0,0 +1,12 @@
+//
+//  CWAdditions.h
+//  UniversalExample
+//
+//  Created by Cory D. Wiles on 6/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+#import "UITabBarController+CWAdditions.h"
---
+++ Classes/UITabBarController+CWAdditions.h
@@ -0,0 +1,13 @@
+//
+//  UITabBarController.h
+//  UniversalExample
+//
+//  Created by Cory D. Wiles on 6/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UITabBarController (UITabBarController)
+
+@end
